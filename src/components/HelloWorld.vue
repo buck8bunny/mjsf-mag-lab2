c42ad5b565f3b6bc757cce768561d24d65c27e90
buck8bunny <monstera211@gmail.com>
2024-09-24 13:41:26 +0300

    Vue.js lab 1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="info-container">
+    <p>
+      <strong>Спеціальність:</strong> Інженерія програмного забезпечення
+    </p>
+    <p>
+      <strong>Курс:</strong> 1й магістратури
+    </p>
+    <p>
+      <strong>Група:</strong> 8.1213
+    </p>
+    <p>
+      <strong>ПІБ:</strong> Хліпітько Артем Олександрович
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.info-container {
+  background-color: #f4f4f9;
+  padding: 20px;
+  border-radius: 8px;
+  max-width: 400px;
+  margin: 0 auto;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+p {
+  font-family: 'Arial', sans-serif;
+  font-size: 1.1em;
+  line-height: 1.5;
+  color: #333;
+  margin-bottom: 10px;
+}
+
+strong {
+  color: #007bff;
+}
+
+</style>
